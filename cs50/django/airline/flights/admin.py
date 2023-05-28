dd7e89a45eb741183458d0bb898737c8c718814f
Unnat Singh <unnatsingh@Unnats-MacBook-Air-2.local>
2023-05-28 10:40:09 +0530

    Manipulate models in admin filter and how to use list_display and filter_horizontal

--- cs50/django/airline/flights/admin.py
+++ cs50/django/airline/flights/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Airport, Flights, Passenger
+
 # Register your models here.
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flights)
-admin.site.register(Passenger)
+admin.site.register(Flights, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
